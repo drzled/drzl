e52c73a71194cb117832467ae2a03e727e1cc33a
Jorge Carballo <jorge@molmo.es>
2023-06-04 10:55:52 +0200

    feat: Implement the InitCommand class
    It generates a manifest file with default values. Also, prompt the user to update the manifest file with their specific configuration.

--- src/Commands/InitCommand.php
+++ src/Commands/InitCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drzl\Commands;
 
+use Drzl\Manifest;
 use Illuminate\Console\Command;
 
 final class InitCommand extends Command
@@ -12,7 +13,26 @@ final class InitCommand extends Command
 
     protected $description = 'Initializes the manifiest file with default values';
 
-    public function handle(): void
+    public function __construct(protected Manifest $manifest)
     {
+        parent::__construct();
+    }
+
+    public function handle(): int
+    {
+        $manifestWasCreated = $this->manifest->update(Manifest::default());
+
+        if (! $manifestWasCreated) {
+            $this->error('Error: Unable to generate manifest file.');
+            $this->line('Please check the file permissions and try again.');
+            $this->line('File location: ' . $this->manifest->path());    
+            return self::FAILURE;
+        }
+        
+        $this->info('Manifest file generated successfully.');
+        $this->line('Please update the manifest file with your deployment configuration.');
+        $this->line('File location: ' . $this->manifest->path());
+        
+        return self::SUCCESS;
     }
 }
