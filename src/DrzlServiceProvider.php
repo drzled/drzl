d4078a570632165b24da8273dce360087a0d20e4
Jorge Carballo <jorge@molmo.es>
2023-06-04 08:48:47 +0200

    Initial Project Structure (#2)
    * feat: Creates the composer.json file
    
    * feat: Create a fresh Laravel Zero application
    
    * feat: Defines an initial Dockerfile
    
    * feat: Add Laravel Zero as dependency
    
    * feat: Use the src folder as the App path
    
    * refactor: Remove the add key on the commands config file
    
    It is not necessary to add commands individually. All the commands will be in the Command folders.
    
    * feat: Adds the skeleton for InitCommnad
    
    This way I validate that all the setup is ready to start implementing the application commands.

---
+++ src/DrzlServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drzl;
+
+use Illuminate\Support\ServiceProvider;
+
+class DrzlServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
