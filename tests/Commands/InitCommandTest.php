d33b9fca5e78e5a6565381119980b9e2f8bc7ae9
Jorge Carballo <jorge@molmo.es>
2023-06-05 14:09:55 +0200

    test: Ensure the manifest file is created by the init command

--- tests/Commands/InitCommandTest.php
+++ tests/Commands/InitCommandTest.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Storage;
+
 it('creates the manifest file', function () {
+     $filename = $this->app->make('manifest')->filename;
+     
      $this->artisan('init')
           ->expectsOutputToContain('Manifest file generated successfully.')
           ->assertSuccessful();
+     
+     expect(Storage::exists($filename))->toBeTrue();
 });
