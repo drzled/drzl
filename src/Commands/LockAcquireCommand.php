ee1acad361751c1f537a7507f8914c3a4c60f762
Jorge Carballo <jorge@molmo.es>
2023-06-06 10:05:00 +0200

    feat: Makes the LockAcquireCommand class

---
+++ src/Commands/LockAcquireCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drzl\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+
+class LockAcquireCommand extends Command
+{
+    protected $signature = 'lock:acquire';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
