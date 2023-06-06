017e98fe51ff24aca2068fda17d2cb0b1e19fc97
Jorge Carballo <jorge@molmo.es>
2023-06-06 15:18:45 +0200

    feat: Implements the lock:acquire command

--- src/Commands/LockAcquireCommand.php
+++ src/Commands/LockAcquireCommand.php
@@ -4,16 +4,38 @@ declare(strict_types=1);
 
 namespace Drzl\Commands;
 
+use Drzl\Manifest;
+use Drzl\Processes\ServerLock;
 use LaravelZero\Framework\Commands\Command;
 
 class LockAcquireCommand extends Command
 {
-    protected $signature = 'lock:acquire';
+    protected $signature = 'lock:acquire
+                            {reason : Helps identify the purpose of the lock}';
 
     protected $description = 'Command description';
 
+    public function __construct(protected readonly Manifest $manifest)
+    {
+        parent::__construct();
+    }
+
     public function handle()
     {
-        //
+        $this->line('Acquiring the servers lock...');
+
+        $processResult = with($this->manifest->primaryServer(), function ($primaryServer) {
+            return ServerLock::on($primaryServer)->getExclusiveLock(
+                config('drzl.lockfile'),
+                $this->argument('reason')
+            );
+        });
+
+        if ($processResult->failed()) {
+            $this->error('Error: Unable to acquire server lock.');
+            return self::FAILURE;
+        }
+
+        $this->info('Server lock has been acquired');
     }
 }
