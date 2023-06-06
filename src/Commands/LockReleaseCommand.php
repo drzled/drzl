64341d1494a970c0875a643394f0157b4647af9e
Jorge Carballo <jorge@molmo.es>
2023-06-06 15:19:00 +0200

    feat: Implements the lock:release command

--- src/Commands/LockReleaseCommand.php
+++ src/Commands/LockReleaseCommand.php
@@ -4,16 +4,33 @@ declare(strict_types=1);
 
 namespace Drzl\Commands;
 
+use Drzl\Manifest;
+use Drzl\Processes\ServerLock;
 use LaravelZero\Framework\Commands\Command;
 
 class LockReleaseCommand extends Command
 {
     protected $signature = 'lock:release';
 
-    protected $description = 'Command description';
+    protected $description = 'Release an exclusive lock on the remote server';
 
-    public function handle()
+    public function __construct(public readonly Manifest $manifest)
     {
-        //
+        parent::__construct();
+    }
+
+    public function handle(): void
+    {
+        $this->line('Releasing the servers lock...');
+
+        $processResult = with($this->manifest->primaryServer(), function ($primaryServer) {
+            return ServerLock::on($primaryServer)->releaseLock(
+                config('drzl.lockfile')
+            );
+        });
+        
+        if ($processResult->successful()) {
+            $this->info('Server lock has been released');
+        }
     }
 }
