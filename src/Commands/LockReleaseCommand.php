0eb31b78bfd06a56b1dc04f28e9a4afdfd13e0f1
Jorge Carballo <jorge@molmo.es>
2023-06-06 10:05:20 +0200

    feat: Makes the LockReleaseCommand class

---
+++ src/Commands/LockReleaseCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drzl\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+
+class LockReleaseCommand extends Command
+{
+    protected $signature = 'lock:release';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
