c3edeac6b039e81f38c3197e392239f695cbe8ab
Jorge Carballo <jorge@molmo.es>
2023-06-06 14:58:22 +0200

    feat: Implements the ProcessRunner::class

---
+++ src/ProcessRunner.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drzl;
+
+use Illuminate\Contracts\Process\ProcessResult;
+use Illuminate\Support\Facades\Process;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+final class ProcessRunner
+{
+    protected ConsoleOutput $output;
+
+    protected Stopwatch $stopwatch;
+
+    public function __construct()
+    {
+        $this->output = app('output');
+
+        $this->stopwatch = new Stopwatch();
+    }
+
+
+    public function __invoke($process): ProcessResult
+    {
+        $server = $process->server;
+
+        return Process::run($process->script(), function($type, $message) use ($server) {
+            $this->output->writeln('');
+            $this->output->writeln("<comment>INFO [{$server}] {$message}</comment>");
+        });        
+    }
+}
