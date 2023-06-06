cc853298afcc01b953eb11bc4134529de574745c
Jorge Carballo <jorge@molmo.es>
2023-06-06 09:22:50 +0200

    feat: Makes the SetupCommand class

---
+++ src/Commands/SetupCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drzl\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+
+class SetupCommand extends Command
+{
+    protected $signature = 'setup';
+
+    protected $description = 'Configuration the app servers';
+
+    public function handle()
+    {
+        //
+    }
+}
