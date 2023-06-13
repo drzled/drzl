293ca574caecb0d4034103afcfca3213be854f8d
Jorge Carballo <jorge@molmo.es>
2023-06-13 16:25:28 +0200

    feat: Implements a withOutput method to set the Command output in the process

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -3,6 +3,7 @@
 namespace Drzl\Commands;
 
 use Drzl\Manifest;
+use Drzl\Processes\ProcessInterface;
 use LaravelZero\Framework\Commands\Command;
 
 abstract class BaseCommand extends Command
@@ -10,5 +11,11 @@ abstract class BaseCommand extends Command
     public function __construct(protected readonly Manifest $manifest)
     {
         parent::__construct();
+    }
+
+    protected function withOutput(ProcessInterface $process, \Closure $callback) {
+        $process->withOutput($this->getOutput());
+
+        $callback($process);
     }
 }
